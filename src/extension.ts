a852378f1fc8a1e0a71af45a9aa8b5221fc2009c
Pokey Rule <pokey.rule@gmail.com>
2021-02-28 22:09:23 +0000

    Initial working version

--- src/extension.ts
+++ src/extension.ts
@@ -61,20 +61,6 @@ export function activate(context: vscode.ExtensionContext) {
       writeHost();
     }
   });
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "command-server.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Command server!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
 
   function writeHost() {
     writeFileSync("/tmp/vscode-host", `localhost:${port}`);
