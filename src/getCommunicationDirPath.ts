f6782a76a426f3033a400d8862f8699b85b24c89
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-01-23 13:56:55 +0000

    more closely match Talon-side code

--- src/getCommunicationDirPath.ts
+++ src/getCommunicationDirPath.ts
@@ -1,18 +1,17 @@
 import { homedir } from "os";
 import { join } from "path";
 
-function getTalonDir(): string {
-  if (process.platform === "win32") {
-    return join(homedir(), "AppData", "Roaming", "talon");
-  } else if (process.platform === "darwin" || process.platform === "linux") {
-    return join(homedir(), ".talon");
-  } else {
-    throw new Error(`Unsupported platform: ${process.platform}`);
-  }
-}
-
 export function getCommunicationDirPath() {
   // NB: See https://github.com/talonhub/community/issues/966 for lots of
   // discussion about this path
-  return join(getTalonDir(), ".comms", "vscode-command-server");
+  if (process.platform === "linux" || process.platform === "darwin") {
+    return join(homedir(), ".talon/.comms/vscode-command-server");
+  } else if (process.platform === "win32") {
+    return join(
+      homedir(),
+      "\\AppData\\Roaming\\talon\\.comms\\vscode-command-server"
+    );
+  } else {
+    throw new Error(`Unsupported platform: ${process.platform}`);
+  }
 }
