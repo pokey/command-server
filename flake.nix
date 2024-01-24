489028dc974234e3d39a0a30548353a145fc13d3
fidgetingbits <fidgetingbits@memeoid.cx>
2024-01-24 19:23:36 +0800

    Remove unfree

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs { inherit system; };
       });
     in
     {
